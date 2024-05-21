72bcafbfa233f85070ac63080dcccc6f35b6e965
nik <lesnikowka@mail.ru>
2024-05-22 02:20:59 +0300

    added funcs

--- ChMLaba2/main.cpp
+++ ChMLaba2/main.cpp
@@ -4,53 +4,6 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
-
-# define M_PI           3.14159265358979323846
-
-
-double rightFuncTest(double x, double y)
-{
-	double a = exp(sin(M_PI * x * y) * sin(M_PI * x * y)) * 0.5;
-	double b = (x * x + y * y);
-	double d = M_PI * M_PI;
-	double c = (-1 - 4 * cos(2 * M_PI * x * y) + cos(4 * M_PI * x * y));
-	return a * b * c * d;
-}
-
-double rightFuncTest2(double x, double y)
-{
-	double a = 2 * M_PI * M_PI * y * y * exp(sin(M_PI * x * y) * sin(M_PI * x * y));
-	double b = 2 * sin(M_PI * x * y) * sin(M_PI * x * y) * cos(M_PI * x * y) * cos(M_PI * x * y) - sin(M_PI * x * y) * sin(M_PI * x * y) + cos(M_PI * x * y) * cos(M_PI * x * y);
-	//double d = ;
-	//double c = ;
-	return a * b;
-}
-
-double boundFunc1Test(double x, double y)
-{
-	return 1;
-}
-
-double boundFunc2Test(double x, double y)
-{
-	return exp(sin(2 * M_PI * x) * sin(2 * M_PI * x));
-}
-
-double boundFunc3Test(double x, double y)
-{
-	return 1;
-}
-
-double boundFunc4Test(double x, double y)
-{
-	return exp(sin(M_PI * y) * sin(M_PI * y));
-}
-
-double testFunc(double x, double y)
-{
-	return exp(sin(y * M_PI * x) * sin(y * M_PI * x));
-}
-
 void main()
 {
 	CustomField::visualizeField(8);
