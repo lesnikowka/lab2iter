635fc09a83740e06bec978d50438f1f252b1c2cb
nik <lesnikowka@mail.ru>
2024-05-22 21:51:16 +0300

    Auto-Commit

--- ChMLaba2/CustomField.h
+++ ChMLaba2/CustomField.h
@@ -19,34 +19,38 @@ public:
 	}
 
 	// если область 5х5, передаем 4 (количество разбиений), после 5х5 идет 9х9
-	static bool isInField(int row, int col, int fieldSize)
+	static bool isInField(int row, int col, int m, int n)
 	{
-		int half = fieldSize / 2;
-		int halfHalf = fieldSize / 4;
+		int halfM = m / 2;
+		int halfHalfM = m / 4;
+		int halfN = n / 2;
+		int halfHalfN = n / 4;
 
-		return row > 0 && row < half && col > 0 && col < fieldSize ||
-			   col < halfHalf && col > 0 && row > 0 && row < 8||
-			   col > half && col < half + halfHalf && row > 0 && row < fieldSize ||
-			   row > half + halfHalf && row < fieldSize && col > 0 && col < half + halfHalf;
+		return row > 0 && row < halfM && col > 0 && col < n ||
+			   col < halfHalfN && col > 0 && row > 0 && row < m||
+			   col > halfN && col < halfN + halfHalfN && row > 0 && row < m ||
+			   row > halfM + halfHalfM && row < m && col > 0 && col < halfN + halfHalfN;
 	}
 
 	// если область 5х5, передаем 4 (количество разбиений), после 5х5 идет 9х9
-	static bool isBound(int row, int col, int fieldSize)
+	static bool isBound(int row, int col, int m, int n)
 	{
-		int half = fieldSize / 2;
-		int halfHalf = fieldSize / 4;
+		int halfM = m / 2;
+		int halfHalfM = m / 4;
+		int halfN = n / 2;
+		int halfHalfN = n / 4;
 
 		return row == 0 ||
 			   col == 0 ||
-			   (row == half || row == half + halfHalf) && col >= halfHalf && col <= half ||
-			   (col == half || col == halfHalf) && row <= half + halfHalf && row >= half ||
-			   row == fieldSize && col <= half + halfHalf ||
-			   col == fieldSize && row <= half ||
-			   col == half + halfHalf && row >= half ||
-			   row == half && col >= half + halfHalf;  
+			   (row == halfM || row == halfM + halfHalfM) && col >= halfHalfN && col <= halfN ||
+			   (col == halfN || col == halfHalfN) && row <= halfM + halfHalfM && row >= halfM ||
+			   row == m && col <= halfN + halfHalfN ||
+			   col == n && row <= halfM ||
+			   col == halfN + halfHalfN && row >= halfM ||
+			   row == halfM && col >= halfN + halfHalfN;  
 	}
 
-	static void visualizeField(int n)
+	static void visualizeField(int m, int n)
 	{
 		std::cout << "  ";
 		for (int i = 0; i <= n; i++)
@@ -54,20 +58,20 @@ public:
 			std::cout << i;
 		}
 		std::cout << "\n";
-		for (int i = 0; i <= n; i++)
+		for (int i = 0; i <= m; i++)
 		{
 			std::cout << i << " ";
 			for (int j = 0; j <= n; j++)
 			{
-				if(isInField(i, j, n))
+				if(isInField(i, j, m, n))
 				{
 					std::cout << "*";
 				}
-				if (isBound(i, j, n))
+				if (isBound(i, j, m, n))
 				{
 					std::cout << "@";
 				}
-				if (!isInField(i, j, n) && !isBound(i, j, n))
+				if (!isInField(i, j, m, n) && !isBound(i, j, m, n))
 				{
 					std::cout << ".";
 				}
---
+++ samples/samples.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../ChMLaba2/CustomField.h"
+int main()
+{
+    CustomField::visualizeField(16,8);
+}
+
